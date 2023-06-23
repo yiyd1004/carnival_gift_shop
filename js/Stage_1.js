57099da15a9940dc53f9077d2731b79432608ea1
Yong Yim <yiyd1004@gmail.com>
2023-06-22 19:54:41 -0700

    Carnival Gift Shop Stage 1 in JS
    Carnival Gift Shop Stage 1 in JS

---
+++ js/Stage_1.js
@@ -0,0 +1,6 @@
+console.log("WELCOME TO THE CARNIVAL GIFT SHOP!\n" +
+            "Hello friend! Thank you for visiting the carnival!\n" +
+            "Here's the list of gifts:\n")
+
+const menu = ["Teddy Bear", "Big Red Ball", "Huge Bear", "Candy", "Stuffed Tiger", "Stuffed Dragon", "Skateboard", "Toy Car", "Basketball", "Scary Mask"]
+menu.forEach(item => console.log(item))
